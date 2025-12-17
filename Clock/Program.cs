4d9208136057c935f71a2f9ffccc42ec59ac10d4
Alexander <pchp250g8@tutamail.com>
2025-12-17 17:38:25 +0300

    Добавьте файлы проекта.

---
+++ Clock/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.CursorVisible = false;
+do
+{
+    DateTime currentTime = DateTime.Now;
+    Console.SetCursorPosition(1, 1);
+    Console.Write("Time:"+currentTime.ToLongTimeString());
+    Thread.Sleep(1000);
+}while (!Console.KeyAvailable);
